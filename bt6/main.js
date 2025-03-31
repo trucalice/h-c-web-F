ee96f8b5f219ea8e23dd44ec84bd02b89274a931
trucalice <nguyenthithanhhuong99992006@gmail.com>
2025-03-31 07:50:38 +0700

    Delete bt6 directory

--- bt6/main.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(str) {
-    let reversedStr = str.split("").reverse().join(""); 
-    return str === reversedStr ? "là chuỗi đối xứng" : "không phải chuỗi đối xứng";
-}
-
-let userInput = prompt("Nhập vào một chuỗi:");
-alert(isPalindrome(userInput));
